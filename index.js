5c780cc40df90cff7e0355c020447464db5c8243
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-06-12 23:08:31 +0700

    ok ma

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ var app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.set("views","./views");
+app.set("views", "./views");
 app.listen(3000, console.log("Server is running..."));
 
-app.get("/", (req,res) => {
+var mang = ["Hello", "Bonjour", "React JS"];
+
+app.get("/", (req, res) => {
     res.render("home");
+});
+app.post("/getnode", (req, res) => {
+    res.send(mang);
+});
+
+app.post("/add", function(){
+    
 });
--- public/app.js
+++ public/app.js
@@ -1,15 +1,68 @@
+var list;
 var Note = React.createClass({
     render: function () {
-        return <div >
+        return <div className="div-note">
             {this.props.children}
         </div>
     }
 });
 
+var List = React.createClass({
+    getInitialState() {
+        list = this;
+        return { mang: [] }
+    },
+    render: function () {
+        return (
+            <div className="div-list">
+                <div id="div-add" ></div>
+                {/*gọi phương thức addDiv*/}
+                <button onClick={addDiv}> Add node </button>
+                {
+                    this.state.mang.map(function (note, index) {
+                        return <Note key={index}>{note} </Note>
+                    })
+                }
+            </div>
+        );
+    },
+    componentDidMount() {
+        var that = this;
+        $.post("/getnode", function (data) {
+            that.setState({ mang: data });
+        });
+    }
+});
+
+var InputDiv = React.createClass({
+    send() {
+        $.post("/add", {
+            note: this.refs.txte.value
+        }, function (data) {
+            list.setState({ mang: data });
+        });
+        list.setState({ mang: list.state.mang.concat(this.refs.txte.value) });
+        //gỡ bỏ form div-add sau khi nhập xong !
+        //ReactDOM.unmountComponentAtNode(document.getElementById("div-add"));
+
+    },
+    render: function () {
+        return <div>
+            <input type="text" ref="txte" placeholder="enter your note" />
+            {/*gọi phương thưc send*/}
+            <button onClick={this.send}>Add </button>
+        </div>
+    }
+});
+
+function addDiv() {
+    ReactDOM.render(<InputDiv />, document.getElementById("div-add"));
+}
+
 ReactDOM.render(
     <div>
         <h1> List Management</h1>
-        <Note> Android </Note>
+        <List />
     </div>,
     document.getElementById('root')
 );
